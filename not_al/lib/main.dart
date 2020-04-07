484e05752a87b5c18d142dec9da15795c518bcf7
onur-akdogan <55892556+onur-akdogan@users.noreply.github.com>
2020-04-07 23:33:11 +0300

    add project complate

---
+++ not_al/lib/main.dart
@@ -0,0 +1,339 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:not_al/kategori_islemleri.dart';
+import 'package:not_al/not_detay.dart';
+import 'package:not_al/utils/database_helper.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'models/kategori.dart';
+import 'models/notlar.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'NoteApp',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: Colors.indigo, accentColor: Colors.indigo),
+      home: NotListesi(),
+    );
+  }
+}
+
+class NotListesi extends StatelessWidget {
+  DatabaseHelper databaseHelper = DatabaseHelper();
+  var _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(
+        actions: <Widget>[
+          PopupMenuButton(
+            icon: Icon(FontAwesomeIcons.archive, color: Colors.black38),
+            itemBuilder: (context) {
+              return [
+                PopupMenuItem(
+                  child: ListTile(
+                    leading: Icon(FontAwesomeIcons.alignLeft),
+                    title: Text("Kategoriler"),
+                    onTap:()=> _kategorilerSayfasinaGit(context),
+                  ),
+                ),
+              ];
+            },
+          ),
+        ],
+        elevation: 0,
+        backgroundColor: Colors.white,
+        title: Text(
+          "NoteApp",
+          style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+              fontStyle: FontStyle.italic),
+        ),
+        centerTitle: true,
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            backgroundColor: Colors.indigo,
+            onPressed: () {
+              kategoriEkleDialog(context);
+            },
+            heroTag: "KategoriEkle",
+            tooltip: "Kategori Ekle",
+            child: Icon(
+              FontAwesomeIcons.tags,
+              size: 15,
+              color: Colors.white,
+            ),
+            mini: true,
+          ),
+          FloatingActionButton(
+            backgroundColor: Colors.indigo,
+            tooltip: "Not Ekle",
+            heroTag: "NotEkle",
+            onPressed: () => _detaySayfasinaGit(context),
+            child: Icon(FontAwesomeIcons.solidEdit),
+          ),
+        ],
+      ),
+      body: Notlar(),
+    );
+  }
+
+  void kategoriEkleDialog(BuildContext context) {
+    var formKey = GlobalKey<FormState>();
+    String yeniKategoriAdi;
+
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (context) {
+          return SimpleDialog(
+            title: Text("Kategori Ekle"),
+            children: <Widget>[
+              Form(
+                key: formKey,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    onSaved: (yeniDeger) {
+                      yeniKategoriAdi = yeniDeger;
+                    },
+                    decoration: InputDecoration(
+                      labelText: "Kategori Adı",
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: (girilenKategoriAdi) {
+                      if (girilenKategoriAdi.length < 3) {
+                        return "En az 3 karakter giriniz";
+                      }
+                    },
+                  ),
+                ),
+              ),
+              ButtonBar(
+                children: <Widget>[
+                  OutlineButton(
+                    borderSide:
+                        BorderSide(color: Theme.of(context).primaryColor),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    color: Colors.red,
+                    child: Text(
+                      "Vazgeç",
+                      style: TextStyle(
+                          color: Colors.red, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  OutlineButton(
+                    borderSide:
+                        BorderSide(color: Theme.of(context).accentColor),
+                    onPressed: () {
+                      if (formKey.currentState.validate()) {
+                        formKey.currentState.save();
+                        databaseHelper
+                            .kategoriEkle(Kategori(yeniKategoriAdi))
+                            .then((kategoriID) {
+                          if (kategoriID > 0) {
+                            _scaffoldKey.currentState.showSnackBar(
+                              SnackBar(
+                                content: Text("Kategori Eklendi"),
+                                duration: Duration(seconds: 3),
+                              ),
+                            );
+                            Navigator.pop(context);
+                          }
+                        });
+                      }
+                    },
+                    color: Colors.green,
+                    child: Text(
+                      "Kaydet",
+                      style: TextStyle(
+                          color: Colors.green, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          );
+        });
+  }
+
+  _detaySayfasinaGit(BuildContext context) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => NotDetay(
+                  baslik: "Yeni Not",
+                )));
+  }
+}
+
+class Notlar extends StatefulWidget {
+  @override
+  _NotlarState createState() => _NotlarState();
+}
+
+class _NotlarState extends State<Notlar> {
+  List<Not> tumNotlar;
+  DatabaseHelper databaseHelper;
+
+  @override
+  void initState() {
+    super.initState();
+    tumNotlar = List<Not>();
+    databaseHelper = DatabaseHelper();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: FutureBuilder(
+        future: databaseHelper.notListesiniGetir(),
+        builder: (context, AsyncSnapshot<List<Not>> snapShot) {
+          if (snapShot.connectionState == ConnectionState.done) {
+            tumNotlar = snapShot.data;
+            sleep(Duration(milliseconds: 2000));
+            return ListView.builder(
+              itemCount: tumNotlar.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.only(bottom: 30),
+                  child: ExpansionTile(
+                    leading: _oncelikIconAta(tumNotlar[index].notOncelik),
+                    title: Text(tumNotlar[index].notBaslik),
+                    children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.all(4),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: <Widget>[
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    "Kategori: ",
+                                    style: TextStyle(
+                                        color: Colors.indigo, fontSize: 18),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    tumNotlar[index].kategoriBaslik,
+                                    style: TextStyle(
+                                        color: Colors.black, fontSize: 18),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[],
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                "İçerik: \n" + tumNotlar[index].notIcerik,
+                                style: TextStyle(
+                                    color: Colors.black, fontSize: 16),
+                              ),
+                            ),
+                            ButtonBar(
+                              children: <Widget>[
+                                FlatButton(
+                                    onPressed: () =>
+                                        _notSil(tumNotlar[index].notID),
+                                    child: Text(
+                                      "Sil",
+                                      style: TextStyle(color: Colors.red),
+                                    )),
+                                FlatButton(
+                                    onPressed: () {
+                                      _detaySayfasinaGit(
+                                          context, tumNotlar[index]);
+                                    },
+                                    child: Text(
+                                      "Düzenle",
+                                      style: TextStyle(color: Colors.green),
+                                    )),
+                              ],
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                );
+              },
+            );
+          } else {
+            return Center(child: Text("Yükleniyor\n\n\n\n\n\n\n\nFrom:ONUR AKDOĞAN"));
+          }
+        },
+      ),
+    );
+  }
+
+  _detaySayfasinaGit(BuildContext context, Not not) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => NotDetay(
+                  baslik: "Notu Düzenle",
+                  duzenlenecekNot: not,
+                )));
+  }
+
+  _oncelikIconAta(int notOncelik) {
+    switch (notOncelik) {
+      case 0:
+        return CircleAvatar(
+          child: Text("AZ", style: TextStyle(color: Colors.white)),
+          backgroundColor: Colors.green.shade200,
+        );
+        break;
+      case 1:
+        return CircleAvatar(
+          child: Text("ORTA", style: TextStyle(color: Colors.white)),
+          backgroundColor: Colors.orange.shade400,
+        );
+
+        break;
+      case 2:
+        return CircleAvatar(
+          child: Text("ACIL", style: TextStyle(color: Colors.white)),
+          backgroundColor: Colors.red.shade700,
+        );
+
+        break;
+    }
+  }
+
+  _notSil(int notID) {
+    databaseHelper.notSil(notID).then((silinenID) {
+      if (silinenID != 0) {
+        Scaffold.of(context)
+            .showSnackBar(SnackBar(content: Text("Notu Sildiniz")));
+        setState(() {});
+      }
+    });
+  }
+}
+
+void _kategorilerSayfasinaGit(BuildContext context) {
+  Navigator.of(context)
+      .push(MaterialPageRoute(builder: (context) => Kategoriler()));
+}
---
+++ not_al/lib/not_detay.dart
@@ -0,0 +1,282 @@
+import 'package:flutter/material.dart';
+import 'package:not_al/models/kategori.dart';
+import 'package:not_al/models/notlar.dart';
+import 'package:not_al/utils/database_helper.dart';
+
+class NotDetay extends StatefulWidget {
+  String baslik;
+  Not duzenlenecekNot;
+
+  NotDetay({this.baslik, this.duzenlenecekNot});
+
+  @override
+  _NotDetayState createState() => _NotDetayState();
+}
+
+class _NotDetayState extends State<NotDetay> {
+  var formKey = GlobalKey<FormState>();
+  List<Kategori> tumKategoriler;
+  DatabaseHelper databaseHelper;
+  int kategoriID;
+  int secilenOncelik;
+  String notBaslik, notIcerik;
+  static var _oncelik = ["Düşük", "Orta", "Yüksek"];
+
+  @override
+  void initState() {
+    super.initState();
+    tumKategoriler = List<Kategori>();
+    databaseHelper = DatabaseHelper();
+    databaseHelper.kategorileriGetir().then((kategoriIcerenMapListesi) {
+      for (Map okunanMap in kategoriIcerenMapListesi) {
+        tumKategoriler.add(Kategori.fromMap(okunanMap));
+      }
+
+      if (widget.duzenlenecekNot != null) {
+        kategoriID = widget.duzenlenecekNot.kategoriID;
+        secilenOncelik = widget.duzenlenecekNot.notOncelik;
+      } else {
+        kategoriID = 1;
+        secilenOncelik = 0;
+      }
+
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomPadding: false,
+      body: Container(
+          child: Column(
+        children: <Widget>[
+          Container(
+            width: 300,
+            height: 100,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 0),
+              child: Center(
+                child: Text(
+                  widget.baslik,
+                  style: TextStyle(
+                      fontSize: 22,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic),
+                ),
+              ),
+            ),
+          ),
+          tumKategoriler.length <= 0
+              ? Center(
+                  child: CircularProgressIndicator(),
+                )
+              : Container(
+                  child: Form(
+                    key: formKey,
+                    child: Column(
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(top: 12.0, left: 12),
+                              child: Text(
+                                "Kategori : ",
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.symmetric(
+                                  vertical: 2, horizontal: 20),
+                              margin: EdgeInsets.only(top: 12),
+                              decoration: BoxDecoration(
+                                border:
+                                    Border.all(color: Colors.teal, width: 1),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10)),
+                              ),
+                              child: DropdownButtonHideUnderline(
+                                child: DropdownButton(
+                                  items: kategoriItemleriOlustur(),
+                                  value: kategoriID,
+                                  onChanged: (secilenKategoriID) {
+                                    setState(() {
+                                      kategoriID = secilenKategoriID;
+                                    });
+                                  },
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextFormField(
+                            initialValue: widget.duzenlenecekNot != null
+                                ? widget.duzenlenecekNot.notBaslik
+                                : "",
+                            validator: (text) {
+                              if (text.length < 3) {
+                                return "En Az 3 Karekter Giriniz !";
+                              }
+                            },
+                            onSaved: (text) {
+                              notBaslik = text;
+                            },
+                            decoration: InputDecoration(
+                              hintText: "Not Başlığını Giriniz",
+                              labelText: "Başlık",
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextFormField(
+                            initialValue: widget.duzenlenecekNot != null
+                                ? widget.duzenlenecekNot.notIcerik
+                                : "",
+                            onSaved: (text) {
+                              notIcerik = text;
+                            },
+                            maxLines: 6,
+                            decoration: InputDecoration(
+                              hintText: "Not İçeriğini Giriniz",
+                              labelText: "İçerik",
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                        ),
+                        Row(
+                          children: <Widget>[
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(top: 12.0, left: 12),
+                              child: Text(
+                                "Öncelik : ",
+                                style: TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.symmetric(
+                                  vertical: 2, horizontal: 20),
+                              margin: EdgeInsets.only(top: 12),
+                              decoration: BoxDecoration(
+                                border:
+                                    Border.all(color: Colors.teal, width: 1),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10)),
+                              ),
+                              child: DropdownButtonHideUnderline(
+                                child: DropdownButton<int>(
+                                  items: _oncelik.map((oncelik) {
+                                    return DropdownMenuItem<int>(
+                                      child: Text(
+                                        oncelik,
+                                        style: TextStyle(fontSize: 24),
+                                      ),
+                                      value: _oncelik.indexOf(oncelik),
+                                    );
+                                  }).toList(),
+                                  value: secilenOncelik,
+                                  onChanged: (secilenOncelikID) {
+                                    setState(
+                                      () {
+                                        secilenOncelik = secilenOncelikID;
+                                      },
+                                    );
+                                  },
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: ButtonBar(
+                            alignment: MainAxisAlignment.spaceEvenly,
+                            mainAxisSize: MainAxisSize.max,
+                            children: <Widget>[
+                              RaisedButton(
+                                splashColor: Colors.red,
+                                padding: EdgeInsets.only(
+                                    top: 20, bottom: 20, left: 30, right: 30),
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: Text("Vazgeç"),
+                                color: Colors.red.shade300,
+                              ),
+                              RaisedButton(
+                                  padding: EdgeInsets.only(
+                                      top: 20, bottom: 20, left: 30, right: 30),
+                                  splashColor: Colors.teal,
+                                  color: Colors.green.shade300,
+                                  onPressed: () {
+                                    if (formKey.currentState.validate()) {
+                                      formKey.currentState.save();
+
+                                      var suan = DateTime.now();
+                                      if (widget.duzenlenecekNot == null) {
+                                        databaseHelper
+                                            .notEkle(Not(
+                                                kategoriID,
+                                                notBaslik,
+                                                notIcerik,
+                                                suan.toString(),
+                                                secilenOncelik))
+                                            .then((kaydedilenNotID) {
+                                          if (kaydedilenNotID != 0) {
+                                            Navigator.pop(context);
+                                          }
+                                        });
+                                      } else {
+                                        databaseHelper
+                                            .notGuncelle(Not.withID(
+                                                widget.duzenlenecekNot.notID,
+                                                kategoriID,
+                                                notBaslik,
+                                                notIcerik,
+                                                suan.toString(),
+                                                secilenOncelik))
+                                            .then((guncellenenID) {
+                                          if (guncellenenID != 0) {
+                                            Navigator.pop(context);
+                                          }
+                                        });
+                                      }
+                                    }
+                                  },
+                                  child: Text(
+                                    "KAYDET",style: TextStyle(
+                                    color: Colors.white,
+                                  )),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+        ],
+      )),
+    );
+  }
+
+  List<DropdownMenuItem<int>> kategoriItemleriOlustur() {
+    return tumKategoriler
+        .map((kategori) => DropdownMenuItem<int>(
+              value: kategori.kategoriID,
+              child: Text(
+                kategori.kategoriBaslik,
+                style: TextStyle(fontSize: 24),
+              ),
+            ))
+        .toList();
+  }
+}
